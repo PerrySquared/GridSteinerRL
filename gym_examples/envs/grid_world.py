8f382254dbb85419522c95dd8cb9efc5f11b65a8
PerrySquared <perrysquaredzz@gmail.com>
2024-04-10 16:34:24 +0300

    expanded observation dictionary, added randomisation control in reset, created basic model

--- gym_examples/envs/grid_world.py
+++ gym_examples/envs/grid_world.py
@@ -6,16 +6,24 @@ import random
 
 
 class GridWorldEnv(gym.Env):
-    metadata = {"render_modes": ["human"], "render_fps": 15}
+    metadata = {"render_modes": ["human"], "render_fps": 1000}
 
-    def __init__(self, render_mode=None, size=10):
+    def __init__(self, render_mode="human", size=10):
         self.size = size  # The size of the square grid
         self.window_size = 512  # The size of the PyGame window
+        
+        self._target_locations_copy = None
+        self._agent_location_copy = None
+        self.random_element = 0
+        self.env_steps = 0
+        self.previous_position = np.array([-1, -1])
         
         self.observation_space = spaces.Dict(
             {
                 "agent": spaces.Box(0, size - 1, shape=(2,), dtype=int),
                 "target": spaces.Box(0, 1, shape=(size*size,), dtype=np.float64),
+                "targets_relative_line": spaces.Discrete(5),
+                "targets_relative_general": spaces.Box(0, 1, shape=(4,), dtype=int)
             }
         )
         
@@ -41,16 +49,25 @@ class GridWorldEnv(gym.Env):
         self.window = None
         self.clock = None
         
+        
+        
     def reset(self, seed=None, options=None):
         self.matrix = np.zeros((self.size,self.size))
         self.steps_counter = 1
         self.iterations = 1
         
-        self._target_locations = self.get_target_locations()
+        if self.env_steps % 200 == 0:    
+            self._target_locations_copy = self.get_target_locations()
+            self.random_element = random.randint(0, len(self._target_locations_copy) - 1)
+                
+            self._agent_location_copy = self._target_locations_copy[self.random_element]        
+            del self._target_locations_copy[self.random_element]
+
+        self.env_steps += 1
+        print(self.env_steps)
         
-        random_element = random.randint(0, len(self._target_locations) - 1)
-        self._agent_location = self._target_locations[random_element]        
-        del self._target_locations[random_element]
+        self._target_locations = self._target_locations_copy[:]
+        self._agent_location = self._agent_location_copy[:]
         
         self.matrix[self._agent_location[0]][self._agent_location[1]] = 1 # make starting _target_location a part of the path
         
@@ -76,6 +93,8 @@ class GridWorldEnv(gym.Env):
         # print(_target_locations)
         return _target_locations
     
+    
+    
     def _move(self, action):       
         # Map the action (element of {0,1,2,3}) to the direction we walk in
         # print("action: ", action)
@@ -90,6 +109,8 @@ class GridWorldEnv(gym.Env):
             self.steps_counter += 1 # if not add 1 to the total wire length
             self.matrix[x][y] = 1
 
+
+
     def check_target_location(self):
         for index in range(len(self._target_locations)):
             if np.array_equal(self._agent_location, self._target_locations[index]):
@@ -99,22 +120,61 @@ class GridWorldEnv(gym.Env):
     
         # print(x,y)
         # print(np.transpose(self.matrix))
-        
+
+    def check_for_target_general(self):
+        target_relative_positions = np.array([0, 0, 0, 0]) # top, bottom, left, right
+        for x in range(10):
+            for y in range(10):
+                if self.matrix[x][y] == 0.5 and y < self._agent_location[1]:
+                    target_relative_positions[0] = 1
+                if self.matrix[x][y] == 0.5 and y > self._agent_location[1]:
+                    target_relative_positions[1] = 1
+        for y in range(10):
+            for x in range(10):
+                if self.matrix[x][y] == 0.5 and x < self._agent_location[0]:
+                    target_relative_positions[2] = 1
+                if self.matrix[x][y] == 0.5 and x > self._agent_location[0]:
+                    target_relative_positions[3] = 1
+                    
+        return target_relative_positions
+    
+    def check_for_target_line(self):
+        for y in range(10):
+            if self.matrix[self._agent_location[0]][y] == 0.5 and y < self._agent_location[1]:
+                return 0 # top
+            if self.matrix[self._agent_location[0]][y] == 0.5 and y > self._agent_location[1]:
+                return 1 # bottom
+        for x in range(10):
+            if self.matrix[x][self._agent_location[1]] == 0.5 and x < self._agent_location[0]:
+                return 2 # left
+            if self.matrix[x][self._agent_location[1]] == 0.5 and x > self._agent_location[0]:
+                return 3 # right
+            
+        return 4
+    
+    
+    
     def game_over_check(self):
-        print("targets: ", self._target_locations)
+        # print("targets: ", self._target_locations)
         reward = 0
         game_over = False
         self.iterations += 1
         
-        if self.steps_counter > 50 or self.iterations > 150:
+        if self.steps_counter > 50 or self.iterations > 100:
             game_over = True
-            reward = -100
+            reward = -1
 
         if len(self._target_locations) == 0:
             game_over = True
-            reward = 250     
+            reward = 2
 
-        return reward - self.steps_counter - self.iterations, game_over, self.steps_counter # score is less with each step, hence 30 / steps_counter
+        if np.array_equal(self.previous_position, self._agent_location):
+            reward -= 0.5
+        self.previous_position = self._agent_location[:]
+
+        return reward - 0.005, game_over, self.steps_counter # score is less with each step, hence 30 / steps_counter
+    
+    
     
     def step(self, action):
         # check if exited
@@ -124,18 +184,18 @@ class GridWorldEnv(gym.Env):
                     pygame.quit()
                     quit()
         # move
-        # print("len: ", len(self._target_locations))
+
         self._move(action) # update the position   
         # reset step reward
         reward = 0
         # add reward for collecting _target_locations
         if self.check_target_location():
-            reward += 50
+            reward = 1
             
         score, terminated, _ = self.game_over_check()
         reward += score
         
-        print(self.matrix)
+        # print(self.matrix)
         
         observation = self._get_obs()
         info = self._get_info()
@@ -150,11 +210,19 @@ class GridWorldEnv(gym.Env):
 
 
     def _get_obs(self):
-        return {"agent": self._agent_location, "target": self.matrix.flatten()}
+        # print({"targets_relative_line": self.check_for_target_line(), "targets_relative_general": self.check_for_target_general()})
+        return {
+            "agent": self._agent_location,
+            "target": self.matrix.flatten(),
+            "targets_relative_line": self.check_for_target_line(),
+            "targets_relative_general": self.check_for_target_general()
+            }
 
     def _get_info(self):
-        return {"agent": self._agent_location, "target": self._target_locations}
-
+        return {
+            "agent": self._agent_location,
+            "target": self._target_locations
+            }
 
 
 
@@ -236,6 +304,8 @@ class GridWorldEnv(gym.Env):
             return np.transpose(
                 np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
             )
+
+
 
     def close(self):
         if self.window is not None:
--- gymtest.py
+++ gymtest.py
@@ -1,13 +1,21 @@
-import gym
-import gym_examples
 
-env = gym.make('gym_examples/GridWorld-v0')
-obs, info = env.reset()
-
-while True:
-    # Take a random action
-    action = env.action_space.sample()
-    obs, reward, done, truncated, info = env.step(action)
-    
-    if done == True:
-        env.reset()
+import gym 
+import gym_examples 
+from stable_baselines3 import A2C 
+ 
+env = gym.make('gym_examples/GridWorld-v0') 
+         
+ 
+model = A2C("MultiInputPolicy", env, verbose=1) 
+model.learn(total_timesteps=1e07) 
+model.save("grid") 
+ 
+del model # remove to demonstrate saving and loading 
+ 
+model = A2C.load("grid") 
+ 
+obs = env.reset() 
+while True: 
+    action, _states = model.predict(obs) 
+    obs, rewards, dones, info = env.step(action) 
+    env.render("human")
